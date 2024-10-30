4538a5a9ba7fd3c1b86add15a942ea77d0b4e0c5
Wolfy Chu <wolfhdragon@gmail.com>
2024-10-30 13:55:29 -1000

    Initial commit

---
+++ Player/scripts/state.gd
@@ -0,0 +1,17 @@
+class_name State extends Node
+
+static var player: Player 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func Enter() -> void:
+	pass
+	
+func Exit() -> void:
+	pass
